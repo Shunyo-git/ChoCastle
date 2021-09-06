884117e292788680ba4fd9e369ed71aca6625d06
Shunyo <85222329+Shunyo-git@users.noreply.github.com>
2021-09-06 17:43:49 +0800

    identity support
    支援認證功能

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChoCastle.Startup))]
+namespace ChoCastle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
